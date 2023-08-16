9302bd66a7bf3edeab23bad05391583b474442f7
i3oi3ka <i3oi3ka@ukr.net>
2023-08-16 21:25:20 +0300

    add image for fruit and add logic end game

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ white_color = pygame.Color(255, 255, 255)
 black_color = pygame.Color(0, 0, 0)
 red_color = pygame.Color(220, 20, 0)
 green_color = pygame.Color(124, 252, 45)
+blue_color = pygame.Color(25, 45, 230)
 
 pygame.init()
 
 display_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 bg = pygame.transform.scale(pygame.image.load('BG.jpg'), (SCREEN_WIDTH, SCREEN_HEIGHT))
+apple = pygame.transform.scale(pygame.image.load('apple.jpg'), (30, 30))
+orange = pygame.transform.scale(pygame.image.load('orange.jpg'), (30, 30))
+pear = pygame.transform.scale(pygame.image.load('pear.jpg'), (30, 30))
+banana = pygame.transform.scale(pygame.image.load('banana.jpg'), (30, 30))
+
+sb = pygame.transform.scale(pygame.image.load('snake_body.jpg'), (10, 10))
+fruit_images = [apple, orange, pear, banana]
+
 pygame.display.set_caption("python - Snake")
 game_clock = pygame.time.Clock()
 
@@ -26,20 +35,52 @@ position_of_fruit = [
     random.randrange(1, (SCREEN_WIDTH // 10)) * 10,
     random.randrange(1, (SCREEN_HEIGHT // 10)) * 10
 ]
-position_of_fruit2 = [
-    random.randrange(1, (SCREEN_WIDTH // 10)) * 10,
-    random.randrange(1, (SCREEN_HEIGHT // 10)) * 10
-]
 
 spawning_of_fruit = True
-spawning_of_fruit2 = True
 
 for i in range(5):
     body_of_snake.append([position_of_snake[0] + 10 * i, position_of_snake[1]])
 initial_direction = 'LEFT'
 snake_direction = initial_direction
 
+player_score = 0
+
+
+def display_score(selection, font_color, font_style, font_size):
+    score_font_style = pygame.font.SysFont(font_style, font_size)
+    score_surface = score_font_style.render("Your Score: " + str(player_score), True, font_color)
+    score_rec = score_surface.get_rect()
+    display_screen.blit(score_surface, score_rec)
+
+
+def game_over():
+    game_over_font_style = pygame.font.SysFont('Verdana', 50)
+    game_over_surface = game_over_font_style.render("Your Score is : " + str(player_score), True, red_color)
+    game_over_rec = game_over_surface.get_rect()
+    game_over_rec.midtop = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    display_screen.blit(game_over_surface, game_over_rec)
+    pygame.display.flip()
+    time.sleep(2)
+    pygame.quit()
+    quit()
+
+
+def game_win():
+    game_win_font_style = pygame.font.SysFont('Verdana', 50)
+    game_win_surface = game_win_font_style.render("Congratulations! Your Score is : 10", True, red_color)
+    game_win_rec = game_win_surface.get_rect()
+    game_win_rec.midtop = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    display_screen.blit(game_win_surface, game_win_rec)
+    pygame.display.flip()
+    time.sleep(2)
+    pygame.quit()
+    quit()
+
+
+random_fruit_image = random.choice(fruit_images)
+
 game_run = True
+
 while game_run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -77,9 +118,8 @@ while game_run:
 
     body_of_snake.insert(0, list(position_of_snake))
     if position_of_snake[0] == position_of_fruit[0] and position_of_snake[1] == position_of_fruit[1]:
+        player_score += 1
         spawning_of_fruit = False
-    elif position_of_snake[0] == position_of_fruit2[0] and position_of_snake[1] == position_of_fruit2[1]:
-        spawning_of_fruit2 = False
     else:
         body_of_snake.pop()
 
@@ -88,19 +128,26 @@ while game_run:
             random.randrange(1, (SCREEN_WIDTH // 10)) * 10,
             random.randrange(1, (SCREEN_HEIGHT // 10)) * 10
         ]
+        random_fruit_image = random.choice(fruit_images)
     spawning_of_fruit = True
 
-    if not spawning_of_fruit2:
-        position_of_fruit2 = [
-            random.randrange(1, (SCREEN_WIDTH // 10)) * 10,
-            random.randrange(1, (SCREEN_HEIGHT // 10)) * 10
-        ]
-    spawning_of_fruit2 = True
-
     for position in body_of_snake:
-        pygame.draw.rect(display_screen, red_color, pygame.Rect(position[0], position[1], 10, 10))
-        pygame.draw.rect(display_screen, red_color, pygame.Rect(position_of_fruit[0], position_of_fruit[1], 10, 10))
-        pygame.draw.rect(display_screen, green_color, pygame.Rect(position_of_fruit2[0], position_of_fruit2[1], 10, 10))
+        display_screen.blit(sb, (position[0], position[1]))
+        display_screen.blit(random_fruit_image, (position_of_fruit[0], position_of_fruit[1]))
+
+    if position_of_snake[0] < 0 or position_of_snake[0] > SCREEN_WIDTH - 10:
+        game_over()
+    if position_of_snake[1] < 0 or position_of_snake[1] > SCREEN_HEIGHT - 10:
+        game_over()
+
+    for block in body_of_snake[1:]:
+        if position_of_snake[0] == block[0] and position_of_snake[1] == block[1]:
+            game_over()
+
+    if player_score == 3:
+        game_win()
+
+    display_score(1, blue_color, 'Verdana', 20)
     pygame.display.update()
     game_clock.tick(speed_of_snake)
 pygame.quit()
